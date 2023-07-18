b50a4fc7d809d5b7b37067c221f5d6a8131badd8
BuildTools <davidmaamoaix@gmail.com>
2023-07-18 21:11:05 +0800

    cleanup

--- include/mini_jvm/class_loader/class.h
+++ include/mini_jvm/class_loader/class.h
@@ -4,7 +4,6 @@
 #include <stdint.h>
 #include <unicode/ustring.h>
 
-#include "mini_jvm/class_loader/class_format.h"
 #include "mini_jvm/constant_pool/constant_pool.h"
 #include "mini_jvm/error/err_vm.h"
 
@@ -34,10 +33,3 @@ typedef struct {
     GHashTable *fields;
     GHashTable *methods;
 } co_cls_obj;
-
-// err_vm co_read_cls_obj(co_cls_obj *cls, cf_cls_file *file);
-
-err_vm co_fprint_name_type(UFILE *out, GPtrArray *cp, cp_name_and_type *entry);
-err_vm co_fprint_constant_pool(UFILE *out, co_cls_obj *obj);
-err_vm co_fprintf_fields(UFILE *out, co_cls_obj *obj);
-err_vm co_fprint_cls_obj(co_cls_obj *obj, FILE *output);
